95f92d32ce92d147ad3ea76f07a0ce55cfa429cc
Sheeza Ali <106429982+sheezaalii@users.noreply.github.com>
2024-05-15 16:56:25 +0500

    1/10 done

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Explore a world of culinary creativity with our diverse menu offerings, guaranteed to satisfy every palate. From comforting classics to bold innovations, our menu promises an unforgettable dining experience.</p>
+            <button>View Menu</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StoreContext = () => {
+  return (
+    <div>StoreContext</div>
+  )
+}
+
+export default StoreContext
