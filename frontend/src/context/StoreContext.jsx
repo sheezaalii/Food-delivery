eb6d4dab9f3217f7f373c2cdf1bb1714b4e03a79
Sheeza Ali <106429982+sheezaalii@users.noreply.github.com>
2024-05-17 23:59:14 +0500

    1.2/10

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,3 +1,18 @@
 import { createContext } from "react";
 
-export const StoreContext = createContext 
+export const StoreContext = createContext (null)
+
+const StoreContextProvider = (props) => {
+
+  const contextValue ={
+    
+
+  }
+  return(
+    <StoreContext.Provider value={contextValue}>
+        {props.childern}
+    </StoreContext.Provider>
+  )
+}
+
+export default StoreContextProvider;
